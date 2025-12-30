565207994b3c7bd1a76e157ccdc8383f428aeb8b
Wassim-RF <wassimrifi0313@gmail.com>
2025-12-30 17:29:07 +0100

    J'ai commancai en clase de repositories tel que j'ai creer les principale methode(creation , modification , deletation d'un user)

--- database/athena.sql
+++ database/athena.sql
@@ -3,8 +3,7 @@ CREATE TABLE users (
     full_name VARCHAR(50) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
-    role ENUM('admin','chef','member') NOT NULL,
-    status ENUM('active','disabled') DEFAULT 'active'
+    role ENUM('admin','chef','member') NOT NULL
 );
 
 CREATE TABLE projects (
---
+++ repositories/userRepositories.php
@@ -0,0 +1,70 @@
+<?php
+    namespace Repositories;
+
+    use Classes\User;
+    use Core\DataBase;
+    use PDO;
+
+    class UserRepositories {
+        private PDO $pdo;
+
+        public function __construct(PDO $pdo) {
+            $db = new DataBase();
+            $this->pdo = $db->connect();
+        }
+        public function createUser(User $user) {
+            $sql = "INSERT INTO users (full_name , email , password , role) VALUES (:full_name , :email , :password , :role)";
+            $stmt = $this->pdo->prepare($sql);
+            $stmt->execute([
+                'full_name' => $user->getFullName(),
+                'email' => $user->getEmail(),
+                'password' => $user->getPassword(),
+                'role' =>$user-> getRole()
+            ]);
+            $id = (int) $this->pdo->lastInsertId();
+            $user->setId($id);
+            return $user;
+        }
+        public function deleteUser(int $id) {
+            $sql = "DELETE FROM users WHERE id = ?";
+            $stmt = $this->pdo->prepare($sql);
+            return $stmt->execute(['id' => $id]);
+        }
+        public function updateUser(User $user) {
+            $sql = "UPDATE users SET full_name = :full_name , email = :email , password = :password , role = :role WHERE id = :id";
+            $stmt = $this->pdo->prepare($sql);
+            return $stmt->execute([
+                'full_name' => $user->getFullName(),
+                'email' => $user->getEmail(),
+                'password' => $user->getPassword(),
+                'role' => $user->getRole(),
+                'id' => $user->getRole()
+            ]);
+        }
+        public function findAllUsers() {
+            $sql = "SELECT * FROM users";
+            $stmt = $this->pdo->prepare($sql);
+            $stmt->execute();
+
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+        public function findUserById(int $id) {
+            $sql = "SELECT * FROM users WHERE id = :id";
+            $stmt = $this->pdo->prepare($sql);
+            $stmt->execute(['id' => $id]);
+
+            return $stmt->fetch(PDO::FETCH_ASSOC);
+        }
+        public function existeEmail(string $email) {
+            $sql = "SELECT COUNT(*) FROM users WHERE email = :email";
+            $stmt = $this->pdo->prepare($sql);
+            $stmt->execute(['email' => $email]);
+            return $stmt->fetchColumn() > 0;
+        }
+        public function existeFullName(string $full_name) {
+            $sql = "SELECT COUNT(*) FROM users WHERE full_name = :full_name";
+            $stmt = $this->pdo->prepare($sql);
+            $stmt->execute(['full_name' => $full_name]);
+            return $stmt->fetchColumn() > 0;
+        }
+    }
