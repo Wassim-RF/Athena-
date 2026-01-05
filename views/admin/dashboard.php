4f25f57f12fe84306b8338bbecad4ddce14f3d8f
Wassim-RF <wassimrifi0313@gmail.com>
2026-01-05 14:17:45 +0100

    feat: j'ai creer la structure du page dashboard d'admine

--- views/admin/dashboard.php
+++ views/admin/dashboard.php
@@ -10,6 +10,23 @@
         <?php
             require_once __DIR__ . '/../../views/layout/header.php';
         ?>
-        <?php var_dump($_SESSION['user']);?>
+        <section class="p-[4%] flex flex-col gap-10">
+            <div>
+                <h2 class="text-2xl font-bold">Dashboard</h2>
+                <p class="text-sm text-gray-400">Aperçu de vos projets et de l'activité de votre équipe...</p>
+            </div>
+            <div class="grid grid-cols-4 gap-6 w-full">
+                <div class="bg-white rounded-2xl p-5 border border-gray-200 shadow-sm h-44"></div>
+                <div class="bg-white rounded-2xl p-5 border border-gray-200 shadow-sm h-44"></div>
+                <div class="bg-white rounded-2xl p-5 border border-gray-200 shadow-sm h-44"></div>
+                <div class="bg-white rounded-2xl p-5 border border-gray-200 shadow-sm h-44"></div>
+            </div>
+            <div class="grid grid-cols-2 gap-6 w-full">
+                <div class="bg-white rounded-2xl p-5 border border-gray-200 shadow-sm h-96"></div>
+                <div class="bg-white rounded-2xl p-5 border border-gray-200 shadow-sm h-96"></div>
+            </div>
+            <div class="w-full h-96 bg-white border border-gray-200 rounded-2xl shadow-sm"></div>
+            <div class="w-full h-96 bg-white border border-gray-200 rounded-2xl shadow-sm"></div>
+        </section>
     </main>
 </body>
--- views/layout/header.php
+++ views/layout/header.php
@@ -1,5 +1,5 @@
 <header class="w-full h-12 bg-white border-b border-gray-100 shadow-[4px_0_24px_rgba(0,0,0,0.05)] sticky top-0 right-0 flex items-center justify-between p-[1%] pt-[2%] pb-[2%]">
-    <div class="group flex items-center text-gray-900 bg-gray-100 rounded-lg border-none p-[0.4%] gap-5 w-[30%]">
+    <div class="group flex items-center text-gray-900 bg-gray-100 rounded-lg border-none p-[0.4%] gap-5 w-[30%] pl-[1%]">
         <svg class="w-5 h-5 text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
         </svg>
@@ -17,12 +17,12 @@
             </span>
         </button>
 
-        <div class="flex items-center gap-3 cursor-pointer border-l border-gray-200 pl-5">
+        <div class="flex items-center gap-3 cursor-pointer border-l border-gray-200 pl-5 p-1 hover:bg-gray-100 hover:border-0">
             <div class="text-right hidden sm:block">
                 <div class="text-sm font-semibold text-gray-900"><?= $_SESSION['user']['full_name'] ?></div>
                 <div class="text-xs text-gray-500"><?= $_SESSION['user']['role'] ?></div>
             </div>
-            <div class="flex items-center justify-center w-10 h-10 rounded-full bg-linear-to-tr from-purple-600 to-indigo-600 text-white font-medium text-lg ring-2 ring-white shadow-sm">
+            <div class="flex items-center justify-center w-7 h-7 rounded-full bg-linear-to-tr from-purple-600 to-indigo-600 text-white font-medium text-lg ring-2 ring-white shadow-sm">
                 <?= $_SESSION['user']['full_name'][0] ?>
             </div>
         </div>
